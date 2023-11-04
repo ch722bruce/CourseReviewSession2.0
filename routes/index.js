e19ea7ee12e241b7e875d44d7c236c6f8b56fa87
Nigel Nie <nieh_yunk@163.com>
2023-11-04 00:26:55 -0700

    file structure fixed

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+import path, { dirname } from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import { fileURLToPath } from "url";
+
+import indexRouter from "./routes/index.js";
+
+import myDB from "../db/myMongoDb.js";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
