4b2e6d158c2ef1e53a752969f2c0f1b8aadffad6
Yunke Nie <nie.yun@northeastern.edu>
2023-12-12 17:08:45 -0800

    modified with lighthouse review revision

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -5,6 +5,12 @@ import "../styles/home.css";
 export function HomePage() {
   return (
     <div className="home-bg">
+      <title>Login</title>
+      <meta
+        name="description"
+        content="Login to access your account and manage your preferences."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <div className="home">
         <Title title="Login" />
         <LoginForm />
